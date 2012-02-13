526b2cf8bf1394ed78c4016f6aee9c3a7a37eb9a
atomicbrainman <atomic@iig-global.com>
2012-02-13 14:37:45 +0200

    inapp-track signature check changed

--- inapp-track.php
+++ inapp-track.php
@@ -4,24 +4,24 @@ require 'includes/master.inc.php';
 
 $res = 'ERR';
 
-$bundle_id = getParams($_REQUEST, 'BundleId', 'str');
-$data['trx_id'] = getParams($_REQUEST, 'TrxId', 'str');
-$data['inapp_id'] = getParams($_REQUEST, 'InappId', 'str');
-$data['trx_date'] = getParams($_REQUEST, 'TrxDate', 'str');
-$data['bundle_version'] = getParams($_REQUEST, 'BundleVersion', 'str');
-$data['price'] = getParams($_REQUEST, 'Price', 'str');
-$data['currency'] = getParams($_REQUEST, 'Currency', 'str');
-$data['uuid'] = getParams($_REQUEST, 'UUID', 'str');
-$sign = getParams($_REQUEST, 'Sign', 'str');
+$bundle_id = getParams($_REQUEST, 'bundleid', 'str');
+$data['trx_id'] = getParams($_REQUEST, 'trxid', 'str');
+$data['inapp_id'] = getParams($_REQUEST, 'inappid', 'str');
+$data['trx_date'] = getParams($_REQUEST, 'trxdate', 'str');
+$data['bundle_version'] = getParams($_REQUEST, 'bundleversion', 'str');
+$data['price'] = getParams($_REQUEST, 'price', 'str');
+$data['currency'] = getParams($_REQUEST, 'currency', 'str');
+$data['uuid'] = getParams($_REQUEST, 'uuid', 'str');
+$sign = getParams($_REQUEST, 'sign', 'str');
 
 if (!empty($bundle_id) && !empty($data['trx_id']) && !empty($data['inapp_id']) && !empty($data['trx_date']) && !empty($data['bundle_version']) 
-		&& isset($_REQUEST['Price']) && !empty($data['currency']) && !empty($data['uuid']) && !empty($sign)) {
+		&& isset($_REQUEST['price']) && !empty($data['currency']) && !empty($data['uuid']) && !empty($sign)) {
 	
 	$app = new Application();
 	$app->select($bundle_id, 'bundle_id'); # Try getting app by bundle id
 	if ($app->ok()) {
 		# check request signature
-		if ($sign == md5($bundle_id.implode($data).$app->custom_salt)) {
+		if ($sign == md5(sprintf("%s.%s.%u.%s", $app->custom_salt, $data['uuid'], $data['trx_date'], $data['trx_id']))) {
 			# we got here through all the checks, now add track to db
 			$inapp = new Inapp();
 			$inapp->trx_id = $data['trx_id'];

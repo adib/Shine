72beb6f1ca33cc9bea22533abecbee4430408b67
atomicbrainman <atomic@iig-global.com>
2012-05-10 19:32:10 +0300

    GetDealy receiver updated
    $_POST => $_REQUEST

--- getdealy-license.php
+++ getdealy-license.php
@@ -2,17 +2,17 @@
 require 'includes/master.inc.php';
 
 # Check required params
-if (empty($_POST['app']) || empty($_POST['email']) || empty($_POST['order'])) {
+if (empty($_REQUEST['app']) || empty($_REQUEST['email']) || empty($_REQUEST['order'])) {
 	error_log("Not enough params for a license request");
 	die("Not enough params for a license request");
 }
 
 ########## APP ###########
 $app = new Application();
-$app->select($_POST['app'], 'getdealy_name'); // custom
+$app->select($_REQUEST['app'], 'getdealy_name'); // custom
 if (!$app->ok()) {
-	error_log("Application {$_POST['app']} not found!");
-	die("Application {$_POST['app']} not found!");
+	error_log("Application {$_REQUEST['app']} not found!");
+	die("Application {$_REQUEST['app']} not found!");
 }
 
 ########## License or default ##########
@@ -26,8 +26,8 @@ if (!$lt->ok()) {
 # Insert Order
 $o = new Order();
 $o->app_id = $app->id;
-$o->txn_id = $_POST['order'];
-$o->payer_email = $_POST['email'];
+$o->txn_id = $_REQUEST['order'];
+$o->payer_email = $_REQUEST['email'];
 $o->payment_gross = $app->getdealy_price;
 $o->quantity = $lt->quantity;
 $ed = $lt->expiration_days;
@@ -38,8 +38,8 @@ $o->type = 'GetDealy';
 $o->generateSerial(); # generates serial into $o->serial_number
 
 # Getting name
-if (!empty($_POST['name'])) {
-	$name = explode(' ', $_POST['name'], 2);
+if (!empty($_REQUEST['name'])) {
+	$name = explode(' ', $_REQUEST['name'], 2);
 	if (!empty($name[1])) {
 		$o->first_name = $name[0];
 		$o->last_name = $name[1];
@@ -47,18 +47,18 @@ if (!empty($_POST['name'])) {
 	else $o->last_name = $name[0];
 }
 else {
-	if (!empty($_POST['first_name'])) $o->first_name = $_POST['first_name'];
-	if (!empty($_POST['last_name'])) $o->last_name = $_POST['last_name'];
+	if (!empty($_REQUEST['first_name'])) $o->first_name = $_REQUEST['first_name'];
+	if (!empty($_REQUEST['last_name'])) $o->last_name = $_REQUEST['last_name'];
 }
 
-if (!empty($_POST['item_name'])) $o->item_name = $_POST['item_name'];
-if (!empty($_POST['country'])) $o->residence_country = $_POST['country'];
-if (!empty($_POST['currency'])) $o->mc_currency = $_POST['currency'];
-if (!empty($_POST['payment_type'])) $o->payment_type = $_POST['payment_type'];
-if (!empty($_POST['tax'])) $o->tax = $_POST['tax'];
-if (!empty($_POST['payer_id'])) $o->payer_id = $_POST['payer_id'];
-if (!empty($_POST['payment_fee'])) $o->payment_fee = $_POST['payment_fee'];
-if (!empty($_POST['notes'])) $o->notes = $_POST['notes'];
+if (!empty($_REQUEST['item_name'])) $o->item_name = $_REQUEST['item_name'];
+if (!empty($_REQUEST['country'])) $o->residence_country = $_REQUEST['country'];
+if (!empty($_REQUEST['currency'])) $o->mc_currency = $_REQUEST['currency'];
+if (!empty($_REQUEST['payment_type'])) $o->payment_type = $_REQUEST['payment_type'];
+if (!empty($_REQUEST['tax'])) $o->tax = $_REQUEST['tax'];
+if (!empty($_REQUEST['payer_id'])) $o->payer_id = $_REQUEST['payer_id'];
+if (!empty($_REQUEST['payment_fee'])) $o->payment_fee = $_REQUEST['payment_fee'];
+if (!empty($_REQUEST['notes'])) $o->notes = $_REQUEST['notes'];
 
 $id = $o->insert();
 

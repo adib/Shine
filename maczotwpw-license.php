ac859a5474fa2ea2bfb40e2afcc54cb8bea66361
egordienko <egordienko89@gmail.com>
2013-03-28 14:20:56 +0200

    maczot license script for WPW

--- maczotwpw-license.php
+++ maczotwpw-license.php
@@ -1,22 +1,4 @@
 <?php
-require 'includes/master.inc.php';
-
-$abbreviations_list = array('WPW');
-
-$app = new Application();
-$app->select('WPW', 'abbreviation'); // custom
-if (!$app->ok()) {
-	error_log("Application WPW not found!");
-	die("Application WPW not found!");
-}
-
-$lt = new LicenseType();
-$lt->select('WPW_MACZOT_01', 'abbreviation'); // custom
-if (!$lt->ok()) {
-	error_log("License type WPW_MACZOT_01 not found!");
-	die("License type WPW_MACZOT_01 not found!");
-}
-
 if (empty($_REQUEST['name'])) {
 	error_log("Name is not set!");
 	die("Name is not set!");
@@ -27,56 +9,31 @@ if (empty($_REQUEST['email'])) {
 	die("Email is not set!");
 }
 
-# FIXME: security? nah, haven't heard of... (MacZOT have no security check ?)
-
-//# Security data collect
-//$check_data = '';
-//ksort($_REQUEST);
-//foreach ($_REQUEST as $key => $val) {
-//	if ($key != 'security_request_hash') $check_data .= $val;
-//}
-//
-//// FastSpring security check...
-//if (md5($check_data . $app->fs_license_key) != $_REQUEST['security_request_hash'])
-//	die('Security check failed.');
-
-$o = new Order();
-$params = array(
-	'type' => 'WPWMacZOT',
-	'app_id' => $app->id,
-	'payer_email' => $_REQUEST['email']
-);
-$o->selectMultiple($params);
-
-# Found
-if ($o->ok()) $response = $o->serial_number;
-else {
-	# Insert Order
-	$o = new Order();
-	$o->app_id = $app->id;
-	$o->payer_email = $_REQUEST['email'];
-	$o->quantity = $lt->quantity;
-	$ed = $lt->expiration_days;
-	if (!empty($ed)) $o->expiration_date = date('Y-m-d', time()+$ed*86400);
-	$o->license_type_id = $lt->id;
-	$o->dt = dater();
-	$o->type = 'WPWMacZOT';
-	$o->generateSerial(); # generates serial into $o->serial_number
-	
-	# Getting name
-	if (!empty($_REQUEST['name'])) {
-		$name = explode(' ', $_REQUEST['name'], 2);
-		if (!empty($name[1])) {
-			$o->first_name = $name[0];
-			$o->last_name = $name[1];
-		}
-		else $o->last_name = $name[0];
-	}
-	
-	$id = $o->insert();
-
-	# Return serial number
-	$response = ($id > 0) ? $o->serial_number : 'Order already exists. Security violation';
+if (empty($_REQUEST['orderID'])) {
+	error_log("OrderID is not set!");
+	die("Order ID is not set!");
 }
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$orderID = $_REQUEST['orderID'];
+
+$security_request_hash = md5($email.$name.$orderID.'719279c718955653ca17889cc6f7629d');
+
+$postRequest = "name=" . $name . "&email=" . $email . "&reference=" . $orderID . "&security_request_hash=" . $security_request_hash;
+
+if( $curl = curl_init() ) {
+    curl_setopt($curl, CURLOPT_URL, 'http://wallwiz.com/secure/fastspringReceiver.php');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $postRequest);
+    $out = curl_exec($curl);
+    echo $out;
+    curl_close($curl);
+  }
+else {
+	$response = 'Connection failed';
+}
+
 
 echo $response;

c0d21edee398333de8fb6ef0367dc0dd1fe9b858
egordienko <egordienko89@gmail.com>
2013-04-04 18:32:26 +0300

    WPW-704 license script for WPW

---
+++ wpw-license.php
@@ -0,0 +1,39 @@
+<?php
+if (empty($_REQUEST['name'])) {
+	error_log("Name is not set!");
+	die("Name is not set!");
+}
+
+if (empty($_REQUEST['email'])) {
+	error_log("Email is not set!");
+	die("Email is not set!");
+}
+
+if (empty($_REQUEST['reference'])) {
+	error_log("Order reference is not set!");
+	die("Order reference is not set!");
+}
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$reference = $_REQUEST['reference'];
+
+$security_request_hash = md5($email.$name.$orderID.'719279c718955653ca17889cc6f7629d');
+
+$postRequest = "name=" . $name . "&email=" . $email . "&reference=" . $reference . "&security_request_hash=" . $security_request_hash;
+
+if( $curl = curl_init() ) {
+    curl_setopt($curl, CURLOPT_URL, 'http://wallwiz.com/secure/fastspringReceiver.php');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $postRequest);
+    $out = curl_exec($curl);
+    echo $out;
+    curl_close($curl);
+  }
+else {
+	$response = 'Connection failed';
+}
+
+
+echo $response;

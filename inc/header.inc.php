e91bcc8b22c37142f6b2d70be2cb5d1c79488152
Sasmito Adibowo <adib@basil-salad.com>
2018-08-13 20:59:05 +0700

    Lock JavaScript library versions.
    Specifically lock JavaScript external references that doesn't have
    any version number in the URL. These libraries were locked:
    
     - Highcharts
     - JQuery
    
    This is to reduce the possibility that things break from JavaScript's
    side. (Since that community tend to have a lax view on backward
    compatibility).
    
    The verssion numbers were inferred from code existing in the repository.
    
    There's a `highcharts.js` file in the repository and that's at version
    `2.1.4` (commit ID `f4b4445132a537404dd84c4a8fe13c6e20c40d57`).
    CloudFlare only has `2.1.5` and `2.1.6`, hence pick `2.1.6` instead.
    
    jQuery was needed mostly by Highcharts. The file `highcharts.js`
    version `2.1.4`was committed on 5 May 2011. At that time, the current
    jQuery version was 1.6.x. Hence take the latest version that is
    available in CloudFlare and it is `1.6.4`.

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -6,8 +6,8 @@
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/yui/2.9.0/reset-fonts-grids/reset-fonts-grids.css" type="text/css">
     <link rel="stylesheet" href="css/yuiapp.css" type="text/css">
-    <script src="//code.jquery.com/jquery.min.js" type="text/javascript"></script>
-    <script src="//code.highcharts.com/highcharts.js" type="text/javascript"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highcharts/2.1.6/highcharts.min.js" type="text/javascript"></script>
 </head>
 <body class="rounded">
     <div id="doc3" class="yui-t6">
